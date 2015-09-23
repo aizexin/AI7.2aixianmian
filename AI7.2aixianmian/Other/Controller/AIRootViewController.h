083476cd681713bae2200c12331d3c7dba8dd73d
aizexin <443792431@qq.com>
2015-09-23 20:31:33 +0800

    完成界面搭建，以及数据库操作封装，数据请求封装

---
+++ AI7.2aixianmian/Other/Controller/AIRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  XLRootViewController.h
+//  iOS7.2爱限免
+//
+//  Created by MS on 15-9-23.
+//  Copyright (c) 2015年 xuli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AIRootViewController : UIViewController
+
+@end
